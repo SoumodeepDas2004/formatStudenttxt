17478aaef7756a6503eb34de0a43017bce288b5b
Soumodeep Das <higherdas09@gmail.com>
2024-07-11 10:30:22 +0530

    Add files via upload

---
+++ formattt.py
@@ -0,0 +1,6 @@
+name=input("Enter your name:")
+marks=input("Enter your Marks:")
+ph_no=input("Enter your phone NUMBER:")
+sentence="The name of student is {},\nHis marks is {}\n& Phone number is {}".format(name,marks,ph_no)
+print(sentence)
+print("\nSuccessfully executed")
